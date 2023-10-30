feefda63d15035d1524d691ba20ff856d16be3f6
Oleksandr <sanyagolikov97@gmail.com>
2023-10-30 18:25:10 +0200

    Navbar

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const  Hero = () => {
+  return (
+    <div className='text-white'>
+      <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col'>
+        <p className='text-[#00df9a]'>Growing with data analytics</p>
+        <h1>Growing with data.</h1>
+        <div>
+          <p>Fast, flexeble financing for</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
